8ad172b0344462f9fc4a1fd6b5b9ef9966609fd6
Eduardo Bouças <mail@eduardoboucas.com>
2020-07-07 21:44:54 +0100

    Update Context type

--- packages/core/src/lib/context.ts
+++ packages/core/src/lib/context.ts
@@ -1,3 +1,25 @@
-type Context = Record<string, any>
+export default class Context {
+  private hash: Map<string, any>
 
-export default Context
+  constructor(initialData?: object) {
+    const hash = new Map(initialData && Object.entries(initialData))
+
+    if (!hash.has('base$cache')) {
+      hash.set('base$cache', new Map())
+    }
+
+    this.hash = hash
+  }
+
+  get(key: string) {
+    return this.hash.get(key)
+  }
+
+  has(key: string) {
+    return this.hash.has(key)
+  }
+
+  set(key: string, value: any) {
+    return this.hash.set(key, value)
+  }
+}
